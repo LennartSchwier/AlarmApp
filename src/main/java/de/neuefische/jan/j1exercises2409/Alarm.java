4d0181f6d74eb105bfa6a15795ab3c1cb58778c1
Lennart <lennart.schwier@icloud.com>
2020-10-01 11:36:18 +0200

    Alarm class simplified by removing getMaximumOfPeople().

--- src/main/java/de/neuefische/jan/j1exercises2409/Alarm.java
+++ src/main/java/de/neuefische/jan/j1exercises2409/Alarm.java
@@ -3,15 +3,12 @@ package de.neuefische.jan.j1exercises2409;
 public class Alarm {
 
     public static String getPeopleCapacityStatus(int numberOfPeople, AlarmLevel threatLevel) {
-        int limit = getMaximumNumberOfPeople(threatLevel);
+        int limit = threatLevel.getMaxCapacity();
         if (numberOfPeople <= limit) {
             return "Maximum number of people not exceeded";
         }
         return "Too many people";
     }
 
-    public static int getMaximumNumberOfPeople(AlarmLevel threatLevel){
-        return threatLevel.getMaxCapacity();
-        }
 }
 
